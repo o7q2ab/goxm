e0075e281fe05b1b97d6f50d4621b5edc5cce215
o7q2ab <o7q2ab@gmail.com>
2024-03-15 20:05:42 +0200

    internal/build: try vsc.revision if version is unknown

--- internal/build/version.go
+++ internal/build/version.go
@@ -1,5 +1,31 @@
 package build
 
+import (
+	"runtime/debug"
+)
+
 var version = "unknown"
 
-func Version() string { return version }
+func Version() string {
+	if version != "unknown" {
+		return version
+	}
+	if c := readCommit(); c != "" {
+		return c
+	}
+
+	return version
+}
+
+func readCommit() string {
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		return ""
+	}
+	for _, s := range bi.Settings {
+		if s.Key == "vsc.revision" && s.Value != "" {
+			return s.Value
+		}
+	}
+	return ""
+}
