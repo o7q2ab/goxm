339fec816ef7e51ba677d3f923d61d87d60d6489
o7q2ab <o7q2ab@gmail.com>
2024-03-21 15:07:03 +0200

    internal/build/version: read main module version before vcs revision

--- internal/build/version.go
+++ internal/build/version.go
@@ -10,17 +10,19 @@ func Version() string {
 	if version != "unknown" {
 		return version
 	}
-	if c := readCommit(); c != "" {
+	if c := readVersion(); c != "" {
 		return c
 	}
-
 	return version
 }
 
-func readCommit() string {
+func readVersion() string {
 	bi, ok := debug.ReadBuildInfo()
 	if !ok {
 		return ""
+	}
+	if bi.Main.Version != "" && bi.Main.Version != "(devel)" {
+		return bi.Main.Version
 	}
 	for _, s := range bi.Settings {
 		if s.Key == "vcs.revision" && s.Value != "" {
